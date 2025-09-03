ccfe07016deeef4141d52afaf54a9801f3d9b2c2
JonathanTubac <javiertubac1290.e@gmail.com>
2025-09-02 23:24:26 -0600

    feat(Property): Added model and repository for property

---
+++ app/src/main/java/uvg/edu/tripwise/data/model/property.kt
@@ -0,0 +1,17 @@
+package uvg.edu.tripwise.data.model
+
+data class Post(
+    val name: String,
+    val description: String,
+    val location: String,
+    val pricePerNight: Int,
+    val Capacity: Int,
+    val pictures: List<String>,
+    val amenities: List<String>,
+    val propertyType: String,
+    val owner: String,
+    val approved: String,
+    val reviews: List<String>,
+    val latitude: Float,
+    val longitude: Float
+)
---
+++ app/src/main/java/uvg/edu/tripwise/data/repository/propertyRepository.kt
@@ -0,0 +1,10 @@
+package uvg.edu.tripwise.data.repository
+
+import uvg.edu.tripwise.network.PropertyApiService
+import uvg.edu.tripwise.network.RetrofitInstance
+
+class propertyRepository (
+    private val api: PropertyApiService = RetrofitInstance.PropertyApi
+){
+    suspend fun getProperties() = api.getProperties()
+}
